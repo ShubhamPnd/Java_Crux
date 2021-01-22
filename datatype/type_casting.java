0c9d91dc8b41ef05f57efde9fba116a638643a86
ShubhamPnd <60984701+ShubhamPnd@users.noreply.github.com>
2021-01-22 23:37:50 +0530

    added type casting

---
+++ datatype/type_casting.java
@@ -0,0 +1,10 @@
+package datatype;
+
+public class type_casting {
+    public static void main(String[] args) {
+        int n;
+        float f = 2.3f;
+        n = (int)f;
+        System.out.println(n);
+    }
+}
