507d59f009344b191a6d0d291e58f6a3ee1bcd49
ShubhamPnd <60984701+ShubhamPnd@users.noreply.github.com>
2021-01-30 21:58:59 +0530

    Create ASCIIatoz.java

---
+++ ASCIIatoz.java
@@ -0,0 +1,10 @@
+public class AsciiValueAtoZ  
+{  
+public static void main(String[] args)   
+{  
+for(int i = 97; i <= 122; i++)  
+{  
+System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+}  
+}  
+}  
