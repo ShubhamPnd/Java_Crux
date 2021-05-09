f410742c90b2aeb5258356dfa21afeab079dd4ea
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-09 23:35:43 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String aString = "Shubham Pandey";
+        System.out.println("Your Name is:" + aString);
+        int age = 20;
+        System.out.println("Your age is:" + age);
+    }
+}
