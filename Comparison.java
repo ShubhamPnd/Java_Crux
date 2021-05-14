1d992342bc3f6157bfc964d2c68a286b00480e71
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-14 23:29:33 +0530

    Comp

---
+++ Comparison.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Comparison {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        System.out.println(num > 98);
+        int result = 7*49/7 + 35/7;
+        System.out.println(result);
+    }
+}
