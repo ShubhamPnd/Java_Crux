53cc78b4818af250f40c86168b61054df5beefeb
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-15 23:50:11 +0530

    literals

---
+++ Literals.java
@@ -0,0 +1,24 @@
+package com.example.java;
+
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        short sh = 65;
+        System.out.println(sh);
+        int i = 6756;
+        System.out.println(i);
+        long lo = 72642624L;
+        System.out.println(lo);
+        float f = 987.3f;
+        System.out.println(f);
+        double d = 242324232.2d;
+        System.out.println(d);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean bool = true;
+        System.out.println(bool);
+        System.out.println(age);
+        String str = "Shubham Pandey";
+        System.out.println(str);
+    }
+}
