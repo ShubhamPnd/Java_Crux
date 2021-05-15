c2e9a9673e2515a9f9be9919bbab9adcd63c4f0e
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-15 23:50:35 +0530

    First

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello this is my first application");
+        int num1 = 19;
+        int num2 = 78;
+        int num3 = 98;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+    }
+}
