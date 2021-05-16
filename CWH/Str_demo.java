c8f62134c19742ea630adf1543a5d0daa1bf84ce
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-16 23:19:04 +0530

    str

---
+++ CWH/Str_demo.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Str_demo {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+
+        System.out.println("Your Name is : " + name);
+    }
+}
