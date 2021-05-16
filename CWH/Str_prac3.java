e92ea11543d82004311ba0e5c4b5f8fb6d98e03e
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-16 23:22:05 +0530

    str_prc

---
+++ CWH/Str_prac3.java
@@ -0,0 +1,36 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Str_prac3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+//        Problem NO .1
+//        String str = scn.nextLine();
+//
+//        String lString = str.toLowerCase();
+//        System.out.println(lString);
+
+        // Problem No.2
+
+//        String text = scn.nextLine();
+//        text = text.replace(" ","_");
+//        System.out.println(text);
+
+        //Problem No. 3
+//        String letter = "Dear <|name|> , Thanks a lot!!!";
+//        letter = letter.replace("<|name|>","Raghav");
+//        System.out.println(letter);
+//
+
+        // Problem No. 4
+//        String myString = "This String contains   double and   triple spaces";
+//        System.out.println(myString.indexOf("  "));
+//        System.out.println(myString.indexOf("   "));
+
+
+        // Problem NO. 5
+        String myLetter = "Dear Shubham , \n\t You are a good guy";
+        System.out.println(myLetter);
+    }
+}
