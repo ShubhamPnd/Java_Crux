855b3bedc9aca7a3809074e37c0cd0991d0152b1
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-16 23:23:42 +0530

    user

---
+++ CWH/User_Name.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class User_Name {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+        System.out.println("Hello," + name + "!" + ",have a Good Day!!" );
+    }
+}
