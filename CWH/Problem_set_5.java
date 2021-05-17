4dc4d5c90f55f08ebe82f906f87439a2fd6e4163
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-17 20:25:49 +0530

    problem_set_5

---
+++ CWH/Problem_set_5.java
@@ -0,0 +1,36 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class Problem_set_5 {
+        public static void main(String[] args) {
+            // Practice Problem 1
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+//        for (int i = n; i > 0 ; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+            // Practice Problem 2:
+//        int sum = 0;
+//        for (int i = 0; i < n; i++) {
+//            sum = sum + (2 * i);
+//        }
+//        System.out.print("Sum of even numbers is: "+" "+ sum);
+
+            // Practice Problem 3:
+//        for (int i = 1; i <=10 ; i++) {
+//            System.out.printf("%d X %d = %d\n" , n,i,n*i);
+//        }
+
+            // Practice Problem 4:
+//            for (int i = 10; i >=1 ; i--) {
+//                System.out.printf("%d X %d = %d\n" , n,i,n*i);
+//            }
+
+s
+    }
+}
