5a8cef8044ea96b09f84f2bc1c94ca21e0bf00f2
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-22 23:42:08 +0530

    void

---
+++ CWH/Void_Return_type.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Void_Return_type {
+    static void tellJoke(){
+        System.out.println("Pappu Badmash Hai!");
+    }
+    public static void main(String[] args) {
+        tellJoke();
+    }
+}
