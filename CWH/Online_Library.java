b362182919e5b22206b5898323937c25e51908da
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-06-05 23:31:59 +0530

    online_LIB

---
+++ CWH/Online_Library.java
@@ -0,0 +1,18 @@
+package Object_oriented_programming;
+
+public class Online_Library {
+    public static void main(String[] args) {
+        /*
+        You have to implement a library using java class library
+        Methods --->
+       1.  issueBook
+       2.  returnBook
+       3.  showAvailableBooks
+       4.  addBook
+        Properties --->
+       1. Array to store the available books
+       2. Array to store the issued books
+
+         */
+    }
+}
