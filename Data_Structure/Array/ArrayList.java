085c1aca84a89f1c65ffc8332df0b6983a773cce
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-12-06 23:38:26 +0530

    Create ArrayList.java

---
+++ Data_Structure/Array/ArrayList.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        ArrayList < Integer > arr = new ArrayList < Integer > ();
+        for (; true;) {
+            int a = inp.nextInt();
+            inp.nextLine();
+            if (a < 0) break;
+            arr.add(a);
+        }
+        inp.close();
+        for (int i = arr.size() - 1; i >= 0; i--)
+            System.out.print(arr.get(i) + " ");
+        System.out.println();
+
+    }
+}
+
+
